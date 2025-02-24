c3e19813bccd7d9b86cb04cdf44a742bfcf3982f
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-02-23 23:01:36 -0400

    refactor and use `lib4bin` hooks

--- gimp-appimage.sh
+++ gimp-appimage.sh
@@ -9,99 +9,62 @@ ICON=gimp.png
 export ARCH="$(uname -m)"
 export APPIMAGE_EXTRACT_AND_RUN=1
 export VERSION="$(pacman -Q $PACKAGE | awk 'NR==1 {print $2; exit}')"
+export STRACE_TIME=15
 
 UPINFO="gh-releases-zsync|$(echo $GITHUB_REPOSITORY | tr '/' '|')|continuous|*$ARCH.AppImage.zsync"
 LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
-URUNTIME="$(wget -q https://api.github.com/repos/VHSgunzo/uruntime/releases -O - \
-	| sed 's/[()",{} ]/\n/g' | grep -oi "https.*appimage.*dwarfs.*$ARCH$" | head -1)"
+URUNTIME=$(wget -q https://api.github.com/repos/VHSgunzo/uruntime/releases -O - \
+	| sed 's/[()",{} ]/\n/g' | grep -oi "https.*appimage.*dwarfs.*$ARCH$" | head -1)
 
 # Prepare AppDir
-mkdir -p ./"$PACKAGE"/AppDir/shared/lib \
-	./"$PACKAGE"/AppDir/usr/share/applications \
-	./"$PACKAGE"/AppDir/etc
-cd ./"$PACKAGE"/AppDir
+mkdir -p ./AppDir/shared/lib ./AppDir/share ./AppDir/etc
+cd ./AppDir
 
-cp -r /usr/share/gimp      ./usr/share
-cp -r /usr/share/locale    ./usr/share
-cp -r /usr/lib/locale      ./shared/lib
-cp -r /usr/share/pixmaps   ./usr/share
-cp -r /etc/gimp            ./etc
+cp -vr /usr/share/gimp      ./share
+cp -vr /usr/share/locale    ./share
+cp -vr /usr/lib/locale      ./shared/lib
+cp -vr /usr/share/pixmaps   ./share
+cp -vr /etc/gimp            ./etc
 
-cp /usr/share/applications/$DESKTOP              ./usr/share/applications
-cp /usr/share/applications/$DESKTOP              ./
-cp /usr/share/icons/hicolor/256x256/apps/"$ICON" ./
+cp /usr/share/applications/"$DESKTOP"             ./
+cp /usr/share/icons/hicolor/256x256/apps/"$ICON"  ./
 
-ln -s ./usr/share  ./share
+ln -s ./           ./usr
 ln -s ./shared/lib ./lib
 ln -s ./"$ICON"    ./.DirIcon
 
 # ADD LIBRARIES
 wget "$LIB4BN" -O ./lib4bin
 chmod +x ./lib4bin
-./lib4bin -p -v -r -s /usr/bin/gimp*
-rm -f ./lib4bin
+xvfb-run -a -- ./lib4bin -p -v -s -k \
+	/usr/bin/gimp* \
+	/usr/lib/libgimp* \
+	/usr/lib/gdk-pixbuf-*/*/* \
+	/usr/lib/gio/*/* \
+	/usr/lib/babl-*/* \
+	/usr/lib/gegl-*/* \
+	/usr/lib/libaa* \
+	/usr/lib/libmng* \
+	/usr/lib/libgs* \
+	/usr/lib/libslang* \
+	/usr/lib/libijs* \
+	/usr/lib/libijs* \
+	/usr/lib/libjbig2dec* \
+	/usr/lib/libgpm* \
+	/usr/lib/libidn* \
+	/usr/lib/libpaper* \
+	/usr/lib/libSDL* \
+	/usr/lib/libXpm.so* \
+	/usr/lib/libheif.so*
 
-cp -nv /usr/lib/libgimp*      ./shared/lib
-cp -nv /usr/lib/libaa*        ./shared/lib
-cp -nv /usr/lib/libmng*       ./shared/lib
-cp -nv /usr/lib/libgs*        ./shared/lib
-cp -nv /usr/lib/libslang*     ./shared/lib
-cp -nv /usr/lib/libijs*       ./shared/lib
-cp -nv /usr/lib/libijs*       ./shared/lib
-cp -nv /usr/lib/libjbig2dec*  ./shared/lib
-cp -nv /usr/lib/libgpm*       ./shared/lib
-cp -nv /usr/lib/libidn*       ./shared/lib
-cp -nv /usr/lib/libpaper*     ./shared/lib
+cp -vn /usr/lib/gegl-*/* ./shared/lib/gegl-*
+cp -rvn /usr/lib/gimp    ./shared/lib
 
 # CREATE APPRUN
-echo '#!/bin/sh
-CURRENTDIR="$(dirname "$(readlink -f "${0}")")"
-
-# set gimp variables
-export GIMP2_DATADIR="$CURRENTDIR"/share/gimp/2.0
-export GIMP2_SYSCONFDIR="$CURRENTDIR"/etc/gimp/2.0
-export GIMP2_LOCALEDIR="$CURRENTDIR"/share/locale
-export GIMP2_PLUGINDIR="$CURRENTDIR"/shared/lib/gimp/2.0
-
-"$CURRENTDIR"/bin/gimp "$@"' > ./AppRun
-chmod +x ./AppRun
-
-# DEPLOY GIMP PLUGINS DEPENDENCIES
-echo "Deploying gimp plugins..."
-cp -rv /usr/lib/gimp     ./shared/lib
-find ./shared/lib/gimp -type f -exec ldd {} \; \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./shared/lib || true
-
-# DEPLOY GDK
-echo "Deploying gdk..."
-GDK_PATH="$(find /usr/lib -type d -regex ".*/gdk-pixbuf-2.0" -print -quit)"
-cp -rv "$GDK_PATH" ./shared/lib
-
-echo "Deploying gdk deps..."
-find ./shared/lib/gdk-pixbuf-2.0 -type f -name '*.so*' -exec ldd {} \; \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./shared/lib || true
-
-find ./shared/lib -type f -regex '.*gdk.*loaders.cache' \
-	-exec sed -i 's|/.*lib.*/gdk-pixbuf.*/.*/loaders/||g' {} \;
-
-# DEPLOY GTK
-echo "Deploying gtk..."
-cp -rv /usr/lib/gtk* ./shared/lib
-
-echo "Deploying gdk deps..."
-find ./shared/lib/gtk* -type f -name '*.so*' -exec ldd {} \; \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./shared/lib || true
-
-find ./shared/lib -type f -regex '.*gdk.*immodules.cache' \
-	-exec sed -i 's|/.*lib.*/gtk.*/.*/3.0.0/||g' {} \;
-
-# DEPLOY WHATEVER THESE ARE
-echo "Deploying the rest of stuff..."
-cp -rv /usr/lib/gio      ./shared/lib
-cp -rv /usr/lib/babl-0.1 ./shared/lib
-cp -rv /usr/lib/gegl-0.4 ./shared/lib
-find ./shared/lib/*/* -type f -name '*.so*' -exec ldd {} \; \
-	| awk -F"[> ]" '{print $4}' | xargs -I {} cp -vn {} ./shared/lib || true
+echo 'GIMP2_DATADIR=${SHARUN_DIR}/share/gimp/2.0
+GIMP2_SYSCONFDIR=${SHARUN_DIR}/etc/gimp/2.0
+GIMP2_LOCALEDIR=${SHARUN_DIR}/share/locale
+GIMP2_PLUGINDIR=${SHARUN_DIR}/shared/lib/gimp/2.0' ./.env
 
 # sharun the gimp plugins
 echo "Sharunning the gimp plugins..."
@@ -118,6 +81,7 @@ cp ./sharun ./shared/lib/gimp/2.0
 	done
 )
 ln -s ../../../ ./shared/lib/gimp/2.0/shared/lib
+ln ./sharun ./AppRun
 ./sharun -g
 
 # MAKE APPIMAGE WITH URUNTIME
@@ -142,8 +106,4 @@ echo "Generating AppImage..."
 
 echo "Generating zsync file..."
 zsyncmake *.AppImage -u *.AppImage
-
-mv ./*.AppImage* ../
-cd ..
-rm -rf ./"$PACKAGE"
 echo "All Done!"
