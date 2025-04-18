1fc3c1d0de60765b28c972f3afaf5a408ba2e362
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-04-18 17:56:26 -0400

    lower block size again since first launch is still slow

--- gimp-appimage.sh
+++ gimp-appimage.sh
@@ -117,7 +117,7 @@ echo "Generating AppImage..."
 	--set-owner 0 --set-group 0 \
 	--no-history --no-create-timestamp \
 	--categorize=hotness --hotness-list=gimp.dwfsprof \
-	--compression zstd:level=22 -S26 -B8 \
+	--compression zstd:level=22 -S25 -B8 \
 	--header uruntime \
 	-i ./AppDir -o ./GIMP-"$VERSION"-anylinux-"$ARCH".AppImage
 
