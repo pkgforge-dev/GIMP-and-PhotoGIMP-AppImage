2369f064f25e773590f98742b4b6b857b4321d8d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-08-26 21:31:17 -0400

    fix wrong package name

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -32,4 +32,4 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
 chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-opengl gtk3-mini opus-mini ffmpeg-mini libxml2-mini llvm-nano
+./get-debloated-pkgs.sh --add-opengl gtk3-mini opus-mini ffmpeg-mini libxml2-mini llvm-libs-nano
