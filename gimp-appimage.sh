6bf8d861baccf7d9f0302832ebb973d1988c5fa3
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-10-15 04:50:48 +0000

    refactor, use `quick-sharun` (#28)

--- gimp-appimage.sh
+++ gimp-appimage.sh
@@ -2,121 +2,95 @@
 
 set -eux
 
-export ARCH="$(uname -m)"
-export APPIMAGE_EXTRACT_AND_RUN=1
-export VERSION="$(cat ~/version)"
-export STRACE_TIME=20
-export OUTNAME=GIMP-"$VERSION"-anylinux-"$ARCH".AppImage
-export OPTIMIZE_LAUNCH=1
-export DESKTOP=/usr/share/applications/gimp.desktop  
-export ICON=/usr/share/icons/hicolor/256x256/apps/gimp.png
-export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
-
-LIB4BN="https://raw.githubusercontent.com/VHSgunzo/sharun/refs/heads/main/lib4bin"
+ARCH="$(uname -m)"
+VERSION="$(cat ~/version)"
+SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
 URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
 PHOTOGIMP="https://github.com/Diolinux/PhotoGIMP/releases/latest/download/PhotoGIMP-linux.zip"
 
-# Prepare AppDir
-mkdir -p ./AppDir/etc ./AppDir/share/icons
-cd ./AppDir
+export ADD_HOOKS="self-updater.bg.hook"
+export STRACE_TIME=15
+export DEPLOY_OPENGL=1
+export DEPLOY_PYTHON=1
+export DEPLOY_LOCALE=1
+export DEPLOY_LIBHEIF=1
+export PYTHON_PACKAGES=PyGObject
+export DESKTOP=/usr/share/applications/gimp.desktop
+export ICON=/usr/share/icons/hicolor/256x256/apps/gimp.png
+export UPINFO="gh-releases-zsync|${GITHUB_REPOSITORY%/*}|${GITHUB_REPOSITORY#*/}|latest|*$ARCH.AppImage.zsync"
+export OUTNAME=GIMP-"$VERSION"-anylinux-"$ARCH".AppImage
+export OPTIMIZE_LAUNCH=1
 
 # ADD LIBRARIES
-wget "$LIB4BN" -O ./lib4bin
-chmod +x ./lib4bin
-xvfb-run -a -- ./lib4bin -p -v -k -s -e -y \
-	--python-pkg PyGObject \
-	/usr/bin/gimp* \
-	/usr/bin/gjs* \
-	/usr/bin/gegl \
-	/usr/lib/libgimp* \
-	/usr/lib/gimp/*/modules/* \
-	/usr/lib/gdk-pixbuf-*/*/*/* \
-	/usr/lib/gtk-*/*/*/* \
-	/usr/lib/gio/*/* \
-	/usr/lib/babl-*/* \
-	/usr/lib/gegl-*/* \
-	/usr/lib/gvfs/* \
-	/usr/lib/libcfitsio.so* \
-	/usr/lib/libgthread-2.0.so* \
-	/usr/lib/libheif/* \
-	/usr/lib/libjbig2dec* \
-	/usr/lib/libgpm* \
-	/usr/lib/libpaper* \
-	/usr/lib/libSDL* \
-	/usr/lib/libXpm.so* \
-	/usr/lib/libheif.so* \
-	/usr/lib/libwmf* \
-	/usr/lib/libudev.so* \
-	/usr/lib/libaa.so* \
+wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
+chmod +x ./quick-sharun
+./quick-sharun \
+	/usr/bin/gimp*               \
+	/usr/bin/gjs*                \
+	/usr/bin/gegl                \
+	/usr/lib/libgimp*            \
+	/usr/lib/gimp/*/modules/*    \
+	/usr/lib/gtk-*/*/*/*         \
+	/usr/lib/gvfs/*              \
+	/usr/lib/libcfitsio.so*      \
+	/usr/lib/libgthread-2.0.so*  \
+	/usr/lib/libjbig2dec*        \
+	/usr/lib/libXmu.so*          \
+	/usr/lib/libwebpdemux.so*    \
+	/usr/lib/libgs.so*           \
+	/usr/lib/libgpm*             \
+	/usr/lib/libpaper*           \
+	/usr/lib/libSDL*             \
+	/usr/lib/libXpm.so*          \
+	/usr/lib/libwmf*             \
+	/usr/lib/libudev.so*         \
+	/usr/lib/libaa.so*           \
 	/usr/lib/libmng.so*
 
-cp -vr /usr/share/gimp           ./share
-cp -vr /usr/share/locale         ./share
-find ./share/locale -type f ! -name '*glib*' ! -name '*gimp*' ! -name '*gegl*' -delete
-cp -vr /usr/lib/locale           ./shared/lib
-cp -vr /usr/share/pixmaps        ./share
-cp -vr /usr/share/icons/hicolor  ./share/icons
-cp -vr /etc/gimp                 ./etc
-cp -vr /usr/share/vala           ./share
-cp -vr /usr/share/gir-1.0        ./share
-cp -vn /usr/lib/gegl-*/*.json    ./shared/lib/gegl-*
-cp -rvn /usr/lib/gimp            ./shared/lib
-
+cp -vr /usr/lib/locale     ./AppDir/shared/lib
+cp -vr /usr/share/pixmaps  ./AppDir/share
+cp -vr /etc/gimp           ./AppDir/etc
+cp -vr /usr/share/vala     ./AppDir/share
+cp -vr /usr/share/gir-1.0  ./AppDir/share
+cp -rvn /usr/lib/gimp      ./AppDir/shared/lib
 
 # sharun the gimp plugins
 echo "Sharunning the gimp plugins..."
-bins_to_find="$(find ./lib/gimp -exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
+bins_to_find="$(find ./AppDir/lib/gimp -exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
 for plugin in $bins_to_find; do
-	mv -v "$plugin" ./shared/bin && ln ./sharun "$plugin"
+	mv -v "$plugin" ./AppDir/shared/bin && ln -f ./AppDir/sharun "$plugin"
 	echo "Sharan $plugin"
 done
 
-# HACK
-find ./lib -type f -name 'libgimpwidgets*' -exec sed -i 's|/usr/share|/tmp/xdg69|g' {} \;
-
-# HACK2
-find ./lib -type f -name 'libgegl*' -exec sed -i 's|/usr/lib|/tmp/o_0|g' {} \;
-echo 'unset GEGL_PATH' > ./.env
-
-# PREPARE SHARUN
-chmod +x ./AppRun
-./sharun -g
-
 # ADD PHOTOGIMP
-wget "$PHOTOGIMP" -O ./PhotoGIMP.zip
+wget --retry-connrefused --tries=30 "$PHOTOGIMP" -O ./PhotoGIMP.zip
 unzip ./PhotoGIMP.zip
 rm -f ./PhotoGIMP.zip
-mv -v ./PhotoGIMP-linux ./PhotoGIMP
-mv -v ./PhotoGIMP/.local/share/applications/org.gimp.GIMP.desktop \
-	./PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop
+mv -v ./PhotoGIMP-linux ./AppDir/PhotoGIMP
+mv -v ./AppDir/PhotoGIMP/.local/share/applications/org.gimp.GIMP.desktop \
+	./AppDir/PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop
 
-if ! grep -q 'StartupWMClass=' ./PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop; then
-	echo 'StartupWMClass=gimp' >> ./PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop
+if ! grep -q 'StartupWMClass=' ./AppDir/PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop; then
+	echo 'StartupWMClass=gimp' >> ./AppDir/PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop
 fi
-if ! grep -q 'TryExec=' ./PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop; then
-	echo 'TryExec=gimp' >> ./PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop
+if ! grep -q 'TryExec=' ./AppDir/PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop; then
+	echo 'TryExec=gimp' >> ./AppDir/PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop
 fi
 
 sed -i -e 's|Exec=.*|Exec=env ENABLE_PHOTO_GIMP=1 gimp %U|g' \
 	-e 's|StartupWMClass=.*|StartupWMClass=gimp|g' \
-	-e 's|TryExec=.*|TryExec=gimp|g' ./PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop
+	-e 's|TryExec=.*|TryExec=gimp|g' ./AppDir/PhotoGIMP/.local/share/applications/PhotoGIMP-AppImage.desktop
 
 # Fix wrong window class in .desktop
-sed -i 's|StartupWMClass=.*|StartupWMClass=gimp|' "$DESKTOP"
-
-# TODO remove me once we migrate to quick-sharun
-cp -v "$DESKTOP" ./
-cp -v "$ICON"    ./
-cp -v "$ICON"    ./.DirIcon
+sed -i 's|StartupWMClass=.*|StartupWMClass=gimp|' ./AppDir/*.desktop
 
 # MAKE APPIMAGE WITH URUNTIME
-cd ..
 wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
 chmod +x ./uruntime2appimage
 ./uruntime2appimage
 
 UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
-wget -O ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH" 
+wget -O ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH"
 chmod +x ./pelf
 echo "Generating [dwfs]AppBundle...(Go runtime)"
 ./pelf --add-appdir ./AppDir \
@@ -124,11 +98,11 @@ echo "Generating [dwfs]AppBundle...(Go runtime)"
 	--appimage-compat \
 	--disable-use-random-workdir \
 	--add-updinfo "$UPINFO" \
-	--compression "-C zstd:level=22 -S25 -B8" \
-	--output-to GIMP-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
+	--compression "-C zstd:level=22 -S26 -B8" \
+	--output-to ./GIMP-"$VERSION"-anylinux-"$ARCH".dwfs.AppBundle
 
 echo "Generating zsync file..."
-zsyncmake *.AppBundle -u *.AppBundle
+zsyncmake ./*.AppBundle -u ./*.AppBundle
 
 mkdir -p ./dist
 mv -v ./*.AppImage*  ./dist
