c31472ad20c059b684948301b64c34764a8cfd67
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-20 04:24:27 -0400

    simplify

--- gimp-appimage.sh
+++ gimp-appimage.sh
@@ -1,18 +1,16 @@
 #!/bin/sh
 
-set -eux
+set -eu
 
 ARCH="$(uname -m)"
 VERSION="$(cat ~/version)"
-SHARUN="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/quick-sharun.sh"
-URUNTIME="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/uruntime2appimage.sh"
-PHOTOGIMP="https://github.com/Diolinux/PhotoGIMP/releases/latest/download/PhotoGIMP-linux.zip"
-
+export ARCH VERSION
 export ADD_HOOKS="self-updater.bg.hook"
 export STRACE_TIME=15
 export DEPLOY_OPENGL=1
 export DEPLOY_PYTHON=1
 export DEPLOY_LOCALE=1
+export DEPLOY_SDL=1
 export DEPLOY_LIBHEIF=1
 export PYTHON_PACKAGES=PyGObject
 export PYTHON_LEAVE_PIP=1
@@ -23,16 +21,15 @@ export OUTNAME=GIMP-"$VERSION"-anylinux-"$ARCH".AppImage
 export OPTIMIZE_LAUNCH=1
 
 # ADD LIBRARIES
-wget --retry-connrefused --tries=30 "$SHARUN" -O ./quick-sharun
-chmod +x ./quick-sharun
-./quick-sharun \
+quick-sharun \
 	/usr/bin/gimp*               \
+	/usr/lib/gimp/*              \
+	/usr/lib/gimp/*/*            \
+	/usr/lib/gimp/*/*/*          \
 	/usr/bin/gjs*                \
 	/usr/bin/gegl                \
 	/usr/lib/libgimp*            \
-	/usr/lib/gimp/*/modules/*    \
 	/usr/lib/gtk-*/*/*/*         \
-	/usr/lib/gvfs/*              \
 	/usr/lib/libcfitsio.so*      \
 	/usr/lib/libgthread-2.0.so*  \
 	/usr/lib/libjbig2dec*        \
@@ -41,29 +38,18 @@ chmod +x ./quick-sharun
 	/usr/lib/libgs.so*           \
 	/usr/lib/libgpm*             \
 	/usr/lib/libpaper*           \
-	/usr/lib/libSDL*             \
 	/usr/lib/libXpm.so*          \
 	/usr/lib/libwmf*             \
 	/usr/lib/libudev.so*         \
 	/usr/lib/libaa.so*           \
-	/usr/lib/libmng.so*
-
-cp -vr /usr/lib/locale     ./AppDir/shared/lib
-cp -vr /usr/share/pixmaps  ./AppDir/share
-cp -vr /etc/gimp           ./AppDir/etc
-cp -vr /usr/share/vala     ./AppDir/share
-cp -vr /usr/share/gir-1.0  ./AppDir/share
-cp -rvn /usr/lib/gimp      ./AppDir/shared/lib
-
-# sharun the gimp plugins
-echo "Sharunning the gimp plugins..."
-bins_to_find="$(find ./AppDir/lib/gimp -exec file {} \; | grep -i 'elf.*executable' | awk -F':' '{print $1}')"
-for plugin in $bins_to_find; do
-	mv -v "$plugin" ./AppDir/shared/bin && ln -f ./AppDir/sharun "$plugin"
-	echo "Sharan $plugin"
-done
+	/usr/lib/libmng.so*          \
+	/usr/share/pixmaps           \
+	/etc/gimp                    \
+	/usr/share/vala              \
+	/usr/share/gir-1.0
 
 # ADD PHOTOGIMP
+PHOTOGIMP="https://github.com/Diolinux/PhotoGIMP/releases/latest/download/PhotoGIMP-linux.zip"
 wget --retry-connrefused --tries=30 "$PHOTOGIMP" -O ./PhotoGIMP.zip
 unzip ./PhotoGIMP.zip
 rm -f ./PhotoGIMP.zip
@@ -86,9 +72,7 @@ sed -i -e 's|Exec=.*|Exec=env ENABLE_PHOTO_GIMP=1 gimp %U|g' \
 sed -i 's|StartupWMClass=.*|StartupWMClass=gimp|' ./AppDir/*.desktop
 
 # MAKE APPIMAGE WITH URUNTIME
-wget --retry-connrefused --tries=30 "$URUNTIME" -O ./uruntime2appimage
-chmod +x ./uruntime2appimage
-./uruntime2appimage
+quick-sharun --make-appimage
 
 UPINFO="$(echo "$UPINFO" | sed 's#.AppImage.zsync#*.AppBundle.zsync#g')"
 wget -O ./pelf "https://github.com/xplshn/pelf/releases/latest/download/pelf_$ARCH"
