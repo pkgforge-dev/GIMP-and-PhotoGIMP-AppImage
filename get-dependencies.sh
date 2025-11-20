d2d01430a05ee310b7034e44a75120b109c4291d
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2025-11-20 05:02:19 -0400

    simplify

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -1,36 +1,27 @@
 #!/bin/sh
 
-set -ex
-EXTRA_PACKAGES="https://raw.githubusercontent.com/pkgforge-dev/Anylinux-AppImages/refs/heads/main/useful-tools/get-debloated-pkgs.sh"
+set -eu
 
 echo "Installing dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	aalib            \
-	alsa-lib         \
-	base-devel       \
-	cfitsio          \
-	curl             \
-	ffmpeg           \
-	ghostscript      \
-	gimp             \
-	git              \
-	gjs              \
-	gvfs             \
-	libheif          \
-	libmng           \
-	librsvg          \
-	strace           \
-	unzip            \
-	wget             \
-	xorg-server-xvfb \
-	zsync
+	aalib       \
+	alsa-lib    \
+	cfitsio     \
+	ffmpeg      \
+	ghostscript \
+	gimp        \
+	git         \
+	gjs         \
+	gvfs        \
+	libheif     \
+	libmng      \
+	librsvg     \
+	unzip
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-wget --retry-connrefused --tries=30 "$EXTRA_PACKAGES" -O ./get-debloated-pkgs.sh
-chmod +x ./get-debloated-pkgs.sh
-./get-debloated-pkgs.sh --add-common --prefer-nano
+get-debloated-pkgs.sh --add-common --prefer-nano
 
 # set version
 pacman -Q gimp | awk '{print $2; exit}' > ~/version
