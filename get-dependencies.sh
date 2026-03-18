d02048fcec39b8bf3b4aaf54e913eefb4c4a77de
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-03-18 09:53:06 -0400

    add `gimp-plugin-gmic`

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,18 +7,19 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-	aalib       \
-	alsa-lib    \
-	cfitsio     \
-	ffmpeg      \
-	ghostscript \
-	gimp        \
-	git         \
-	gjs         \
-	gvfs        \
-	libheif     \
-	libmng      \
-	librsvg     \
+	aalib            \
+	alsa-lib         \
+	cfitsio          \
+	ffmpeg           \
+	ghostscript      \
+	gimp             \
+	gimp-plugin-gmic \
+	git              \
+	gjs              \
+	gvfs             \
+	libheif          \
+	libmng           \
+	librsvg          \
 	unzip
 
 echo "Installing debloated packages..."
